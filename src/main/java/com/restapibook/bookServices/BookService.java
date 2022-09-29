bffd007dbdbdbd7a77f4fe315981a4754bb30113
Ramshek <orientbits@gmial.com>
2022-09-29 08:57:53 +0530

    @GetMapping get books methods completed

---
+++ src/main/java/com/restapibook/bookServices/BookService.java
@@ -0,0 +1,37 @@
+package com.restapibook.bookServices;
+
+import com.restapibook.entities.Book;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BookService {
+    static List<Book> list = new ArrayList<>();
+
+    static {
+        list.add(new Book(123,"Complete Java Course","XYZ"));
+        list.add(new Book(124,"Complete Python Course","PQR"));
+        list.add(new Book(125,"Complete C++ Course","ABC"));
+        list.add(new Book(126,"Complete Dart Course","LMN"));
+    }
+
+    public List<Book> getAllBooks(){
+        return list;
+    }
+
+    public Book getBookById(int id){
+        Book book1 = list.stream().filter(book -> book.getId()==id).findFirst().get();
+
+        return book1;
+    }
+
+    public Book addBook(Book book){
+        list.add(book);
+        return book;
+    }
+
+
+
+}
---
+++ src/main/java/com/restapibook/controllers/BookController.java
@@ -0,0 +1,39 @@
+package com.restapibook.controllers;
+
+import com.restapibook.bookServices.BookService;
+import com.restapibook.entities.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    // get all books handler
+    @GetMapping("/books")
+    public List<Book> getBooks(){
+        return this.bookService.getAllBooks();
+    }
+
+    // get a books handler
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") int id){
+        return bookService.getBookById(id);
+    }
+
+    // add books handler
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book){
+        return this.bookService.addBook(book);
+    }
+
+
+
+
+
+
+}
